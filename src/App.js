036f4c5380541edffc414e013191e5f499d5cb17
Alen John <alen@lilacinfotech.com>
2021-12-23 12:01:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  firstName: yup.string().required("First Name should be required please"),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  age: yup
+    .number()
+    .positive()
+    .integer()
+    .typeError("Please Enter the Age")
+    .required(""),
+  password: yup.string().min(4).max(15).required(),
+  confirmPassword: yup.string().oneOf([yup.ref("password"), null]),
+});
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <input
+          type="text"
+          name="firstName"
+          {...register("firstName", { required: true })}
+          placeholder="First Name..."
+        />
+        <p> {errors.firstName?.message} </p>
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name..."
+          {...register("lastName", { required: true })}
+        />
+        <p> {errors.lastName?.message} </p>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email..."
+          {...register("email", { required: true })}
+        />
+        <p> {errors.email?.message} </p>
+        <input
+          type="text"
+          name="age"
+          placeholder="Age..."
+          {...register("age", { required: true })}
+        />
+        <p> {errors.age?.message} </p>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password..."
+          {...register("password", { required: true })}
+        />
+        <p> {errors.password?.message} </p>
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password..."
+          {...register("confirmPassword", { required: true })}
+        />
+        <p> {errors.confirmPassword && "Passwords Should Match!"} </p>
+        <input type="submit" id="submit" />
+      </form>
     </div>
   );
 }
